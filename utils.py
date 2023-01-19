a1c4af28cc2f27ae6e9ae7ee234049ba2224b144
thibautvalour <74783392+thibautvalour@users.noreply.github.com>
2023-01-19 22:37:10 +0100

    ...

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import csv
+
 import numpy as np
 import scipy.sparse as sp
 
@@ -58,26 +60,41 @@ def load_data():
     """  
     graph_indicator = np.loadtxt("data/graph_indicator.txt", dtype=np.int64)
     _,graph_size = np.unique(graph_indicator, return_counts=True)
+
+    edges = np.loadtxt("data/edgelist.txt", dtype=np.int64, delimiter=",")
+    edges_inv = np.vstack((edges[:,1], edges[:,0]))
+    edges = np.vstack((edges, edges_inv.T))
+    s = edges[:,0]*graph_indicator.size + edges[:,1]
+    idx_sort = np.argsort(s)
+    edges = edges[idx_sort,:]
+    edges,idx_unique =  np.unique(edges, axis=0, return_index=True)
+
     x = np.loadtxt("data/node_attributes.txt", delimiter=",")
     edge_attr = np.loadtxt("data/edge_attributes.txt", delimiter=",")
-    edges = np.loadtxt("data/edgelist.txt", dtype=np.int64, delimiter=",")
-    
-    A = sp.csr_matrix((np.ones(edges.shape[0]), (edges[:,0], edges[:,1])), shape=(graph_indicator.size, graph_indicator.size))
-    A.setdiag(0)
-    A += A.T
+    edge_attr = np.vstack((edge_attr,edge_attr))
+    edge_attr = edge_attr[idx_sort, :]
+    edge_attr = edge_attr[idx_unique, :]
 
-    W = sp.csr_matrix((1/edge_attr[:, 0], (edges[:,0], edges[:,1])), shape=(graph_indicator.size, graph_indicator.size)) # edge weights
+    A = sp.csr_matrix((np.ones(edges.shape[0]), (edges[:,0], edges[:,1])),
+                        shape=(graph_indicator.size, graph_indicator.size))
+    A.setdiag(0)
+    # A += A.T
+
+    weights = edge_attr[:, 0]
+    weights[weights != 0] = 1/weights[weights != 0]
+    W = sp.csr_matrix((weights, (edges[:,0], edges[:,1])),
+                        shape=(graph_indicator.size, graph_indicator.size)) # edge weights
     W.setdiag(0)
-    W += W.T
+    # W += W.T
 
     F = sp.csr_matrix(((edge_attr[:, 1]
-        + edge_attr[:, 2]
-        + edge_attr[:, 3]
-        + edge_attr[:, 4]
-    ), (edges[:,0], edges[:,1])), shape=(graph_indicator.size, graph_indicator.size)) 
+                            + edge_attr[:, 2] *10
+                            + edge_attr[:, 3] *100
+                            + edge_attr[:, 4] *1000 
+        ), (edges[:,0], edges[:,1])), shape=(graph_indicator.size, graph_indicator.size)) 
     F.setdiag(0)
-    F += F.T
-    
+    # F += F.T
+
     adj = []
     adj_weight = []
     Flist = []
@@ -86,9 +103,9 @@ def load_data():
     idx_n = 0
     idx_m = 0
     for i in range(graph_size.size):
-        adj.append(A[idx_n:idx_n+graph_size[i],idx_n:idx_n+graph_size[i]])
-        adj_weight.append(W[idx_n:idx_n+graph_size[i],idx_n:idx_n+graph_size[i]])
-        Flist.append(F[idx_n:idx_n+graph_size[i],idx_n:idx_n+graph_size[i]])
+        adj.append(A[idx_n:idx_n+graph_size[i], idx_n:idx_n+graph_size[i]])
+        adj_weight.append(W[idx_n:idx_n+graph_size[i], idx_n:idx_n+graph_size[i]])
+        Flist.append(F[idx_n:idx_n+graph_size[i], idx_n:idx_n+graph_size[i]])
         edge_features.append(edge_attr[idx_m:idx_m+adj[i].nnz,:])
         features.append(x[idx_n:idx_n+graph_size[i],:])
         idx_n += graph_size[i]
@@ -108,3 +125,21 @@ def normalize_adjacency(A, W):
     L_rw = sp.identity(n) - D_inv.dot(W)
 
     return L_rw
+
+
+def submit_predictions(proteins_test, y_pred_proba,
+                       csv_file='sample_submission.csv'):
+    """
+    Function that writes predictions to a file
+    """
+    with open(csv_file, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        lst = list()
+        for i in range(18):
+            lst.append('class'+str(i))
+        lst.insert(0, "name")
+        writer.writerow(lst)
+        for i, protein in enumerate(proteins_test):
+            lst = y_pred_proba[i,:].tolist()
+            lst.insert(0, protein)
+            writer.writerow(lst)
--- utils/loss.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import torch
-from torch import nn
-
-pops = np.array([440.,  50., 939.,  60., 112., 625., 202.,  74., 998.,  57.,  43.,305.,  44.,  59., 548., 226.,  60.,  46.])
-weights = 1/pops
-weights = weights/np.mean(weights)
-weights = torch.tensor(weights, dtype=torch.float32)
-criterion = nn.CrossEntropyLoss(weight=weights)
