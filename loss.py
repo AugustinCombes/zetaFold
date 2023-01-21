2b3a5b25fdc3741f76bc307738a15e1ccd7b0c10
Thibaut Valour <74783392+thibautvalour@users.noreply.github.com>
2023-01-21 16:40:34 +0100

    Update loss.py
    Define balanced_criterion

--- loss.py
+++ loss.py
@@ -6,4 +6,4 @@ pops = np.array([440.,  50., 939.,  60., 112., 625., 202.,  74., 998.,  57.,  43
 weights = 1/pops
 weights = weights/np.mean(weights)
 weights = torch.tensor(weights, dtype=torch.float32)
-criterion = nn.CrossEntropyLoss(weight=weights)
+balanced_criterion = nn.CrossEntropyLoss(weight=weights)
